98770c90d5443bed28147c09b8077592a0e20d9b
gashayee <tewodroslemma64@gmail.com>
2021-07-23 21:47:23 +0300

    controllers created

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Driver extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'licence',
+        'working_route',
+        'driver_type',
+        'car_owner',
+        'password',
+    ];
+
 }
