1edde7b5d46aae0037e9359e34547c290098ff3c
gashayee <tewodroslemma64@gmail.com>
2021-07-30 21:21:47 +0300

    starters for the tpolice controller functionality

--- app/Http/Controllers/TPoliceController.php
+++ app/Http/Controllers/TPoliceController.php
@@ -16,5 +16,59 @@ class TPoliceController extends Controller
             'driver' => $traffic, 
         ]);
     }
+
+    public function checkDriver(Request $request){
+
+        $validate = $request->validate([
+            'licence' => 'required|string',
+        ]);
+
+        $driver = Driver::where('licence', $request->licence)->first();
+
+            // TO DO
+        // Retrieve the from where/when the driver departed 
+        // Return weather the driver has start it's journy from the bus station
+
+        return response()->json([
+            'Message' => ' 200 Success!',
+            'driver' => $driver,
+        ]);
+
+    }
+
+    public function fineDriver(Request $request){
+
+        $validate = $request->validate([
+            'licence' => 'required|string',
+            'violation' => 'required|string',
+        ]);
+
+        $traffic = $request->user(); 
+
+        // TO DO
+        // Create a fine (Fine table should have a foreign key to driver, tpolice)
+
+        return response()->json([
+            'Message' => '200 Successuly complited',
+        
+        ]);
+
+    } 
     
+    public function previousCrimeRecord(Request $request){
+
+        $validate = $request->validate([
+            'licence' => 'required|string', 
+        ]);
+
+        // TO DO
+        // Retrieve all the fines the driver has paid
+
+        return response()->json([
+            'Message' => '200 Successfuly',
+            
+        ]);
+
+    }
+
 }
