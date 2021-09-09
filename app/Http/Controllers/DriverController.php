a1907edfca3bf9a94a13c7c3d6643c463f276c63
gashayee <tewodroslemma64@gmail.com>
2021-09-09 19:57:52 +0300

    check if ticket is cancled or not

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -24,28 +24,7 @@ class DriverController extends Controller
 
     
     ## validating the cnaceled boolean is creating an issue
-
-    public function latestTicket(Request $request){
-
-        $validate = $request->validate([
-            'licence' => 'required|string',
-        ]);
-
-        $driver = Driver::where('licence', $request->licence)->first();
-        
-       
-        $matchThese = ['driver_id' => $driver->id];
-
-        $lastTicket = Ticket::where($matchThese)->whereDate('created_at', date('Y-m-d'))->get();
-        return response()->json([
-            'Latest ticket' => $lastTicket,
-        ]);
-    }
-
     public function buyTicket(Request $request){
-        
-        //$driver = $request->user();
-        
         
         $validate = Validator::make($request->all(), [ 
             'starting_point' => 'required|string',
@@ -63,11 +42,22 @@ class DriverController extends Controller
                 'error' => $errors->all(),
             ]);
         }
+
+        $matchThese = ['driver_id' => $request->driver_id];
+
+        $lastTicket = Ticket::where($matchThese)->latest()->first();
+        
+        if($lastTicket->canceled == 0){
+            return response([
+                'Message' => 'Cancle || finish your destination',
+                'lastTicket' => $lastTicket,
+            ]);
+        }        
         
         $ticket = Ticket::create($request->all());
 
         return response([
-            'Message' => 'Ticket created successfuly',
+            'Message' => 'Ticket created successfuly', 
             'Ticket' => $ticket,
         ]);
 
